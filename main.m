d3a18821d001c945e97e465395f25afdbecc2ecf
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-05-31 00:19:29 +0530

    Delete main.m

--- main.m
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "MFSAppDelegate.h"
-
-int main(int argc, char *argv[]) {
-	@autoreleasepool {
-		return UIApplicationMain(argc, argv, nil, NSStringFromClass(MFSAppDelegate.class));
-	}
-}
